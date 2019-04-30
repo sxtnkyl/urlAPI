540ef343c583710a20dbd5393ccca15f64d029ed
sxtnkyl <sxtnkyl@gmail.com>
2019-04-30 18:43:50 -0400

    updoots

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//package format
 const express = require("express");
+const dotenv = require("dotenv");
+//file import format
 const data = require("./fakeData");
 
 const app = express();
+dotenv.config();
 
 //the middleware to run between REQUEST and RESPONSE
 app.use("/", (req, res, next) => {
   console.log("request got");
+  console.log(process.env.TEST);
   res.send(data);
 });
 
